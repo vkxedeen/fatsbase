920b2d7024b96f1ecfde5a5c1e8426c3c7d6ef27
vkxedeen <kutuzov.xo@gmail.com>
2018-10-10 22:24:53 +0300

    upd

--- merge.js
+++ merge.js
@@ -16,29 +16,38 @@ promise.then(result => {
   });
 });
 
+result = merge (arr1, arr2)
+
 function compare(obj1, arr) {
-  //obj1 – приоритет. arr – второй массив
-  let obj = Object.assign({}, obj1);
-  arr.forEach(item => {
-    if (obj1.name == item.name) {
-      for (let key in item) {
-        if (obj1[key] * 0.95 > item[key] || obj1[key] * 1.05 < item[key]) {
-          console.log("разница для " + obj1.name + " более 5% в " + key);
-          obj["needCheck"] = true;
-          obj[key] = item[key];
+  //obj1 – проверяемое значение. arr – приоритетный массив
+  let obj = {"needCheck": true}
+  if ( arr.some(item => item.name == obj1.name) ) {
+    arr.forEach(item => {
+      if (item.name == obj1.name) {
+        for (let key in item) {
+          obj[key] = item[key]
+          if (obj1[key] * 0.95 > item[key] || obj1[key] * 1.05 < item[key]) {
+            console.log("разница для " + obj1.name + " более 5% в " + key);
+          }
         }
-      }
-    }
-  });
-  return obj;
+      } 
+     
+    })
+    return obj
+  }
+  else return obj1 
 }
 
 function merge(arr1, arr2) {
-  let arr = [];
+  let arr = arr1.slice();
 
-  for (let i = 0; i < arr1.length; i++) {
-    result = compare(arr1[i], arr2);
-    if (result.needCheck) arr.push(result, arr1[i]);
+  for (let i = 0; i < arr2.length; i++) {
+    result = compare(arr2[i], arr1);
+    if (result.needCheck) {
+      let newArr = arr.filter(item => item.name != result.name)
+      newArr.push(result)
+      arr = newArr.slice()
+    }
     else arr.push(result);
   }
   return arr;
