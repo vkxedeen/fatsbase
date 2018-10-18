da2677755e3029ccbc8506ff9e6995e53d33a3a5
vkxedeen <kutuzov.xo@gmail.com>
2018-10-18 20:56:50 +0300

    Updates

--- precache-manifest.17eebd943fb7242a840e6bf9c6646db4.js
+++ precache-manifest.17eebd943fb7242a840e6bf9c6646db4.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/fatsbase/static/js/runtime~main.ec1e136a.js"
   },
   {
-    "revision": "f95a33a6b5df00a71d57",
-    "url": "/fatsbase/static/js/main.f95a33a6.chunk.js"
+    "revision": "a42325acb39eeb84f5cf",
+    "url": "/fatsbase/static/js/main.a42325ac.chunk.js"
   },
   {
     "revision": "4785994fe77e93368d7d",
     "url": "/fatsbase/static/js/1.4785994f.chunk.js"
   },
   {
-    "revision": "550b6a01dbaaafa67416618ec0a53558",
+    "revision": "196a0af0357f7986904605f473ade13a",
     "url": "/fatsbase/index.html"
   }
 ];
