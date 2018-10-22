0869f9a3578c0be285fcd8e0b431a076ba1ac351
vkxedeen <kutuzov.xo@gmail.com>
2018-10-22 19:26:46 +0700

    with node update mistake

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+    newData: null}
   }
 
   componentDidMount() {
+    debugger
     let self = this;
     fetch("./data.json")
       .then(function(response) {
@@ -22,17 +24,36 @@ class App extends React.Component {
       .catch(error => console.log(error.message));
   }
 
+  findByPattern = (event) => {
+    let obj = this.state.data.slice()
+    if (event.target.value && event.keyCode === 13) {
+
+      let regex = new RegExp(event.target.value)
+      debugger
+      let filtered = obj.filter(item =>  regex.test(item.name) == true )
+      this.setState({newData: filtered
+        })
+      console.log(this.state.newData)  
+
+    }
+  }
+
   render() {
     if (this.state.data) {
       return (
+        <div>
+        <p><b>Find:</b><br/>
+        <input type="text" size="40" value={this.state.inputValue} onKeyUp={(event) => this.findByPattern(event)}/>
+       </p>
         <HashRouter basename="/fatsbase">
           <div>
             <Switch>
               <Route path="/:name" component={ElemInfo} />
-              <Diagram data={this.state.data} />
+              <Diagram data={this.state.data} newData={this.state.newData}/>
             </Switch>
           </div>
         </HashRouter>
+        </div>
       );
     } else return null;
   }
--- src/Diagram.js
+++ src/Diagram.js
@@ -1,34 +1,53 @@
 import React from "react";
 import { addChart } from "./addChart";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Diagram extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { data: this.props.data };
+    this.state = {};
     this.nodes = [];
   }
+  /*componentDidUpdate() {
+    debugger
+    for (let i = 0; i < this.props.newData.length; i++) {
+      addChart(this.nodes[i], this.props.newData[i]);
+    }
+  }
+  */
 
   componentDidMount() {
     for (let i = 0; i < this.props.data.length; i++) {
       addChart(this.nodes[i], this.props.data[i]);
     }
   }
-  shouldComponentUpdate() {
+
+  componentWillUnmount() {
+    this.nodes = [];
+  }
+
+  /*shouldComponentUpdate() {
+    if (this.props.newData != this.props.data) return true
     return false;
   }
+  */
 
   render() {
     if (this.props.data) {
+      let bars = this.props.newData || this.props.data;
       return (
-        <div id="container" style={{ width: 400 + "px" }}>
-          {this.props.data.map((item, i) => (
-            <div>
-              <span><Link to={`/${item.name}`}>{item.name}</Link></span>
-              <div id={i} key={i} ref={node => this.nodes.push(node)}></div>
-            </div>
-          ))}
+        <div>
+          <div id="container" style={{ width: 400 + "px" }}>
+            {bars.map((item, i) => (
+              <div>
+                <span>
+                  <Link to={`/${item.name}`}>{item.name}</Link>
+                </span>
+                <div id={i} key={i} ref={node => this.nodes.push(node)} />
+              </div>
+            ))}
+          </div>
         </div>
       );
     } else {
