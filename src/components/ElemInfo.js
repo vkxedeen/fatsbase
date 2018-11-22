fb3de8d9bf2503204d5f720187a79cf14e96310d
vkxedeen <kutuzov.xo@gmail.com>
2018-11-22 21:13:38 +0700

    upd

--- src/components/ElemInfo.js
+++ src/components/ElemInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const R = require("ramda");
+import * as R from "ramda";
 
 function ElemInfo(props) {
   let { data, match } = props;
