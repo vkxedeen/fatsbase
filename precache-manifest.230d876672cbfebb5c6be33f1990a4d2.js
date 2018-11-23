25067501b52013fd944d94334c2036710bc15d8d
vkxedeen <kutuzov.xo@gmail.com>
2018-11-23 17:25:26 +0700

    Updates

--- precache-manifest.230d876672cbfebb5c6be33f1990a4d2.js
+++ precache-manifest.230d876672cbfebb5c6be33f1990a4d2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/fatsbase/static/js/runtime~main.ec1e136a.js"
   },
   {
-    "revision": "74364479036a131add31",
-    "url": "/fatsbase/static/js/main.74364479.chunk.js"
+    "revision": "6778ff66094ea636dd78",
+    "url": "/fatsbase/static/js/main.6778ff66.chunk.js"
   },
   {
     "revision": "0a5a4c1e66fd3d2a05a6",
     "url": "/fatsbase/static/js/1.0a5a4c1e.chunk.js"
   },
   {
-    "revision": "97921316ba00bb634cb91afa14e93334",
+    "revision": "2197b2c75a88dc237dcd633f31d5f7e7",
     "url": "/fatsbase/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/fatsbase/precache-manifest.37eefa93198fbb395fdaab1c734f6f23.js"
+  "/fatsbase/precache-manifest.230d876672cbfebb5c6be33f1990a4d2.js"
 );
 
 workbox.clientsClaim();
